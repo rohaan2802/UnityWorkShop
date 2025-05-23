1c05d9351efdd12a3a72ff16751b1e36fd000827
M.Rohaan28 <127793299+rohaan2802@users.noreply.github.com>
2025-05-23 06:26:02 +0500

    ks

---
+++ Assets/character controller/NewMonoBehaviourScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal")>-2)
+        {
+            transform.position = transform.position + new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+
+        }
+        
+        if(Input.GetAxis("Vertical")>-2)
+        {
+            transform.position = transform.position + new Vector3(0,0,Input.GetAxis("Vertical") * Time.deltaTime);
+
+        }
+    }
+}
